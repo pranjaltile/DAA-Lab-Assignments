6297f239fb4939a94e40c8aa944d2e5890f8db75
pranjaltile <152598446+pranjaltile@users.noreply.github.com>
2024-11-10 23:05:00 +0530

    Update Search.java

--- Search.java
+++ Search.java
@@ -1,13 +1,36 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Search {
+public class search {
     public static void main(String... args) {
-        int a[] = {1, 7, 12, 23, 31, 42, 45};
-        int beg = 0, end = a.length - 1;
-        int mid;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter number:");
+        // Step 1: Get the size of the array from the user
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();
+        int[] a = new int[size];
+
+        // Step 2: Input array elements from the user
+        System.out.println("Enter " + size + " elements:");
+        for (int i = 0; i < size; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        // Sort the array before performing binary search
+        Arrays.sort(a);
+
+        // Display the sorted array
+        System.out.print("The sorted array is: ");
+        for (int num : a) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // Step 3: Binary search
+        int beg = 0, end = a.length - 1;
+        int mid;
+
+        System.out.println("Enter number to search:");
         int num = sc.nextInt();
 
         boolean found = false;
@@ -28,6 +51,7 @@ public class Search {
         if (!found) {
             System.out.println("Element is not in array");
         }
+
+        sc.close();
     }
 }
-
